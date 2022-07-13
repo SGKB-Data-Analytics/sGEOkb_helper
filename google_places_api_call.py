54235e48c17c028d365c1e18956ef9792f25625b
SGKB-Data-Analytics <98168646+SGKB-Data-Analytics@users.noreply.github.com>
2022-07-13 13:24:37 +0200

    Update google_places_api_call.py
    change google_maps_api_url from geocode to place/textsearch
    change params 'adress' to 'query'

--- google_places_api_call.py
+++ google_places_api_call.py
@@ -6,27 +6,26 @@ import proxysetting as ps
 import google_key as gk
 
 #google Stuff
-GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
+GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
 
 path_to_csv = '../ADRESSEN_ENRICHMENT/DATA/BETRIEB/POS_Stand_20211008.csv'
-path_to_csv_out = '../ADRESSEN_ENRICHMENT/OUTPUT/resultat_alle_google_abfragen_20211008.csv'
+path_to_csv_out = '../ADRESSEN_ENRICHMENT/OUTPUT/resultat_alle_google_abfragen_20220713.csv'  ##############
 
 adressen = pd.read_csv(path_to_csv, header=0, delimiter=";", quoting=csv.QUOTE_NONE, encoding='utf-8' )
-
 #print(adressen)
 
 alle_google_abfragen = []
-for index, row in adressen.head(500).iterrows():
+for index, row in adressen.head(500).iterrows():  ############
     time.sleep(0.1)
     adresse = row['Ort']
     print(adresse)
     params = {    
-        'address': adresse, #'BIBIM SHACK, ZUERICH', # row['Adressen'],
-        'sensor': 'false',
+        'query': adresse, #'BIBIM SHACK, ZUERICH', # row['Adressen'],
         'key': gk.key
         }
 
     resp = requests.get(GOOGLE_MAPS_API_URL, params=params, verify=False, proxies={"http":str( ps.proxysetting),"https":str( ps.proxysetting)})
+  
     #print(resp.status_code)
     if resp.status_code == 200:
         google_abfrage = resp.json()
@@ -38,5 +37,6 @@ for index, row in adressen.head(500).iterrows():
 alle_google_abfragen = pd.concat(alle_google_abfragen)
 
 alle_google_abfragen.to_csv(path_to_csv_out)
+
 
  
